d75c8af00d587c8d986c62c12a1a320763c07084
vwheatle <vwheatle@kent.edu>
2024-11-19 13:53:31 -0500

    more info

--- logutils.h
+++ logutils.h
@@ -21,8 +21,8 @@ typedef struct {
 } LogPerson;
 
 typedef struct {
-	uint32_t timestamp; // should be within 0-3fffffff
-	uint32_t room_id;   // should be within 0-3fffffff or UINT32_MAX for entire building
+	uint32_t timestamp; // within 0-3f..f
+	uint32_t room_id;   // within 0-3f..f, or UINT32_MAX for entire building
 	LogPerson person;
 	LogEventType event;
 } LogEntry;
@@ -37,8 +37,21 @@ typedef struct {
 	size_t length;
 	LogEntry *entry;
 } LogEntries;
+// STARTLOG and ENDLOG markers are not in LogEntries vec
 
-LogFile read_logfile(const char* filename);
+LogFile *logfile_read(const char *);
+// ENDLOG is only checked for, not added to entries. if there's too many, don't
+// care if not found, error, return NULL, panic
 
-void logentry_push(LogEntries *);
+void logfile_write(const char *, const LogFile *);
+// appends ENDLOG transparently
+
+void logentry_push(LogEntries *, LogEntry);
 LogEntry logentry_pop(LogEntries *);
+// it's vaguely vec-like
+
+/*
+token is a password, we also have
+SL_PUBLIC and SL_PRIVATE env vars
+which will be generated and when a new log is created, it will use those keys.
+*/
