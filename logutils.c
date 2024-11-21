f2ca738e98f5774b22cd4eeb0aeb6732ba0a674d
vwheatle <vwheatle@kent.edu>
2024-11-20 21:35:59 -0500

    more input validation

--- logutils.c
+++ logutils.c
@@ -1,7 +1,7 @@
 #include <stddef.h> // -> size_t, ptrdiff_t
 #include <stdio.h>
+#include <ctype.h> // -> isalnum
 #include <string.h>
-#include <time.h>
 
 #include "common.h"
 #include "logutils.h"
@@ -79,27 +79,32 @@ LogArgs parse_args(size_t args_len, char *args[]) {
 }
 
 const char *validate_token(char *given_token) {
+	if (given_token == NULL) return "token is required";
 	char curr;
 	while ((curr = *(given_token++)) != '\0') {
 		// (A-Z)(a-z)(0-9) no spaces case sensitive
-		// return nice error messages
+		if (!isalnum(curr))
+			return "tokens must only have alphanumeric characters";
 	}
 	return NULL;
 }
 
 const char *validate_name(char *name) {
+	if (name == NULL) return "name is required";
 	char curr;
 	while ((curr = *(name++)) != '\0') {
 		// (A-Z)(a-z) no spaces case sensitive
-		// return nice error messages so maybe have separate ones for spaces and
-		// numbers and any other.
-		// and check logargs_validate for example of bringing other errors
-		// forward
+		if (curr == ' ') return "spaces are not allowed in name";
+		if (isdigit(curr)) return "numbers are not allowed in name";
+		if (!isalpha(curr))
+			return "names must only have alphabetical characters and no spaces";
 	}
 	return NULL;
 }
 
 const char *logentry_validate(LogEntry *entry) {
+	const char *msg;
+
 	if (entry->timestamp & 0xC0000000)
 		return "timestamp (-T <timestamp>) is required";
 	if ((entry->room_id & 0xC0000000) && (entry->room_id != UINT32_MAX))
@@ -110,18 +115,23 @@ const char *logentry_validate(LogEntry *entry) {
 	if (entry->event == '\0')
 		return "arrival event (-A) or departure event (-L) required";
 
+	if ((msg = validate_name(entry->person.name)) != NULL) return msg;
+
 	return NULL;
 }
 
 const char *logargs_validate(LogArgs *args) {
-	{ // LogArgs itself has a LogEntry inside it. validate that.
-		const char *msg = logentry_validate(&args->entry);
-		if (msg != NULL) return msg;
-	}
+	const char *msg;
 
-	if (args->given_token == NULL) return "token (-K <token>) required";
+	// LogArgs itself has a LogEntry inside it. validate that.
+	if ((msg = logentry_validate(&args->entry)) != NULL) return msg;
+
 	if (args->log_file == NULL)
 		return "log file name required (no flag, just put it as a string)";
+
+	// (and also validate the token too)
+	if (args->given_token == NULL) return "token (-K <token>) required";
+	if ((msg = validate_token(args->given_token)) != NULL) return msg;
 
 	return NULL;
 }
@@ -129,7 +139,8 @@ const char *logargs_validate(LogArgs *args) {
 LogFile *logfile_read(char *filename, char *given_token) {
 	FILE *file = fopen(filename, "r");
 	if (file == NULL) {
-		printf("\033[0;31mERROR: Unable to open file '%s' \033[0;37m \n", filename);
+		printf("\033[0;31mERROR: Unable to open file '%s' \033[0;37m \n",
+			filename);
 		return NULL;
 	}
 
